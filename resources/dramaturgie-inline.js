d84632d44ef6fe146ed911f16d108f11e4872230
Dr. Michael R. Alvers <m.alvers@ganata.ownprovider.com>
2026-04-30 09:04:50 +0200

    intro scripting

---
+++ resources/dramaturgie-inline.js
@@ -0,0 +1,47 @@
+/** Preloaded dramaturgie for file:// (fetch often fails); mirror resources/dramaturgie.txt if you add it. */
+window.__INTRO_DRAMATURGIE_TXT = `
+#
+120pt
+DOC ALVERS 
+MATHE-
+LABORE
+16pt
+von Dr. Michael R. Alvers
+
+#
+200pt
+38 
+60pt
+Interaktive Mathematik
+Labore
+
+
+# 
+60pt
+5 Programmiersprachen 
+involviert ...
+40pt
+hypertext markup language
+cascading styles sheets
+java script
+python
+JAVA
+
+# 
+200pt
+30.000
+60pt
+Programmzeilen
+
+#
+für Euch!
+%video https://coverr.co/videos/welcome-back-to-school-classrooms-first-lesson
+
+#
+100pt
+das crazy!
+180pt
+%emoji 😎
+%button zurück
+%stop
+`.trim();
