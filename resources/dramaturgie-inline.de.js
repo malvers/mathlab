2db63ea3736101a7603caf2e09d6bc2d7fecdc61
Dr. Michael R. Alvers <m.alvers@ganata.ownprovider.com>
2026-05-01 14:15:05 +0200

    Dramaturgie pro Sprache (de/en/es/fr/it/pt); Intro-Ladevorgang; Labor-Suche CSS; UI/i18n-Anpassungen

---
+++ resources/dramaturgie-inline.de.js
@@ -0,0 +1,81 @@
+/**
+ * Referenz-Dramaturgie (Deutsch). Du pflegst nur diese Datei inhaltlich.
+ * Übersetzungen: dramaturgie-inline.<lang>.js — nach Änderungen hier syncen lassen.
+ * Aufruf intro: ?lang=en (auch es, fr, it, pt). Sonst Browser-Sprache oder de.
+ */
+window.__INTRO_DRAMATURGIE_TXT = `
+
+# 
+80pt
+DOC ALVERS 
+120pt
+MATHE-
+LABORE
+16pt
+von Dr. Michael R. Alvers
+
+#
+200pt
+38 
+60pt
+Interaktive Mathematik
+Labore
+
+#
+120pt
+6 Sprachen
+20pt
+%purplepulsating and counting
+40pt
+deutsch
+englisch
+spanisch
+französisch
+italienisch
+portugiesisch
+
+#
+// es werden alle screenshots gezeigt
+%pause 500
+%screenshots
+%pause 500
+
+# 
+60pt
+7 Programmiersprachen 
+40pt
+hypertext markup language
+cascading styles sheets
+type script
+java script
+python
+JSON
+JAVA
+
+# 
+200pt
+%loc
+60pt
+Programmzeilen
+
+#
+für Euch!
+%picture resources/kids.jpeg
+%sound resources/kids.wav
+//%video https://coverr.co/videos/welcome-back-to-school-classrooms-first-lesson
+
+#
+100pt
+das crazy!
+180pt
+%emoji 😎
+%button zurück
+
+#
+%cwstack left
+120pt
+docalvers.de
+
+%stop
+
+`.trim();
