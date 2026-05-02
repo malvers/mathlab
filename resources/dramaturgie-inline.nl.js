ef500f979954e36bce7071ba3dffa803383ced22
Dr. Michael R. Alvers <m.alvers@ganata.ownprovider.com>
2026-05-02 17:55:18 +0200

    Netherlands

---
+++ resources/dramaturgie-inline.nl.js
@@ -0,0 +1,79 @@
+/** Intro dramaturgy — Nederlands (sync opbouw met dramaturgie-inline.de.js). */
+window.__INTRO_DRAMATURGIE_TXT = `
+
+#notranslate
+80pt
+DOC ALVERS 
+120pt
+MATHE-
+LABORE
+16pt
+Dr. Michael R. Alvers
+
+#
+200pt
+38 
+60pt
+Interactieve wiskunde
+labs
+
+#
+120pt
+8 talen
+20pt
+%purplepulsating en het worden er meer
+30pt
+Duits
+Engels
+Spaans
+Frans
+Italiaans
+Portugees
+Swahili
+Nederlands
+
+#
+// alle screenshots worden getoond
+%pause 500
+%screenshots
+%pause 500
+
+# 
+60pt
+7 technologieën
+40pt
+hypertext markup language
+cascading styles sheets
+type script
+java script
+python
+JSON
+JAVA
+
+# 
+200pt
+%loc
+60pt
+regels code
+
+#
+Voor jullie!
+%picture resources/kids.jpeg
+%sound resources/kids.wav
+//%video https://coverr.co/videos/welcome-back-to-school-classrooms-first-lesson
+
+#
+100pt
+dat is gek!
+180pt
+%emoji 😎
+%button terug
+
+#
+%cwstack left
+120pt
+docalvers.de
+
+%stop
+
+`.trim();
